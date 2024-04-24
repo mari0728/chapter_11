ce1d1fd89e90d0096786ac4830eeacfe2919dc56
mari0728 <gmhmmorales0728@gmail.com>
2024-04-24 15:55:16 +0800

    first commit

---
+++ lib/network/recipe_service.dart
@@ -0,0 +1,31 @@
+import 'dart:developer';
+import 'package:http/http.dart';
+
+const String apiKey = 'c36669385af0a3106c9dedfd6f26def7';
+const String apiId = 'e15571f2';
+const String apiUrl = 'https://api.edamam.com/search';
+
+class RecipeService {
+  // 1
+  Future getData(String url) async {
+    // 2
+    final response = await get(Uri.parse(url));
+    // 3
+    if (response.statusCode == 200) {
+      // 4
+      return response.body;
+    } else {
+      // 5
+      log(response.body);
+    }
+  }
+
+  // 1
+  Future<dynamic> getRecipes(String query, int from, int to) async {
+    // 2
+    final recipeData = await getData(
+        '$apiUrl?app_id=$apiId&app_key=$apiKey&q=$query&from=$from&to=$to');
+    // 3
+    return recipeData;
+  }
+}
